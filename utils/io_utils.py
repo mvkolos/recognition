4ff4afc60ba4c0c8cbdab0c707f602608af6b1e5
mvkolos <Maria.Kolos@skoltech.ru>
2019-10-20 19:09:54 +0300

    yaml substitution

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,9 +1,13 @@
 import yaml 
+import hiyapyco
 
 def save_yaml(what, where):
     with open(where, 'w') as f:
         f.write(yaml.dump(what, default_flow_style = False))
         
 def load_yaml(where):
-    with open(where, 'r') as f:
-        return yaml.load(f)
+    # with open(where, 'r') as f:
+    #    config = hiyapyco.load(f, interpolate=True)
+    config = hiyapyco.load(where, interpolate=True)
+
+    return config
